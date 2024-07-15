99118e4a1800fe46a078e2ab24b2f23f182eeb63
Evan Nagle <evan@mantle.co>
2024-07-15 11:09:33 -1000

    api key

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/17MQEEByjWrjE2S4Tqmgymea4s4XA6_CPkvDq_mUGsZA/edit?gid=70869349#gid=70869349';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyD56ZnYo6WqN6MjvF909hPJLo9WcdJgW54';
